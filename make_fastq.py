3d948eea742992193644b69be2505cf4736cf686
Bradley J. Main <bjmain@users.noreply.github.com>
2020-06-03 11:09:02 -0700

    Update make_fastq.py
    Made the code more user-friendly.

--- make_fastq.py
+++ make_fastq.py
@@ -1,30 +1,30 @@
+import sys
+
+read_len = 100
+step_size = 100
+
 D={}
-# this is the pseudohap genome after filter by sequniq.
-for line in open("aws_pseudohap_uniq_apr.fa"):
-        #for line in open("10x_tig233502.fa"):
+# input fasta file after the python script: python make_fastq.py species.fa
+for line in open(sys.argv[1]):
         i=line.strip().split()
         if line[0]==">":
                 c=i[0].strip(">")
                 D[c]=[]
-                #if len(D)>10:
-                #    break
                 continue
         D[c].extend(line.strip())
 
-# D Key = contig #
-# D values = list of 60mers for each contig
+
+
 contig_list = {}
 for cont in D:
     seq = "".join(D[cont]) # make one contig 
-    # print 600bp reads with 300bp step
-    seq_list = [seq[i:i+600] for i in xrange(0, len(seq), 300)] # len(seq) allows for the last read to be less than 100bp
+    seq_list = [seq[i:i+read_len] for i in xrange(0, len(seq), step_size)] # len(seq) allows for the last read to be less than 100bp
     count=0
     for fragment in seq_list:
         count+=1
         name = "".join([">",cont,"_",str(count)])
         print(name)
         print(fragment)
-        #print "+"
-        #fake_quality_cut = "J" * int(len(cut))
-        #print fake_quality_cut
-    
+        print "+"
+        fake_quality = "J" * int(len(fragment))
+        print fake_quality   # Be careful with this!
